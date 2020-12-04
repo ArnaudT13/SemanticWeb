b3da3675fbee09f621353fc772c895c8bc5ccc82
ArnaudT13 <tavernierarnaud42@gmail.com>
2020-12-04 21:52:48 +0100

    Add station properties

--- pages_web/index.php
+++ pages_web/index.php
@@ -5,42 +5,59 @@
 
     \EasyRdf\RdfNamespace::set('geo', 'http://www.w3.org/2003/01/geo/wgs84_pos#');
     \EasyRdf\RdfNamespace::set('evcs', 'http://www.example.org/chargingontology#');
+    \EasyRdf\RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+    \EasyRdf\RdfNamespace::set('igeo', 'http://rdf.insee.fr/def/geo#');
+    
 
-    $sparql = new EasyRdf\Sparql\Client('http://10.0.2.2:3030/charging_station/sparql');
+    $sparqlChargingStation = new EasyRdf\Sparql\Client('http://10.0.2.2:3030/charging_station/sparql');
+    $sparqlINSEE = new EasyRdf\Sparql\Client('http://rdf.insee.fr/sparql');
 ?>
 <html>
 <head>
   <title>EasyRdf Basic Sparql Example</title>
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>EasyRdf Basic Sparql Example</h1>
+<h1>Stations</h1>
 
-<h2>List of countries</h2>
 <table>
     <thead>
         <tr>
             <th>Station</th>
+            <th>Operateur</th>
             <th>Longitude</th>
             <th>Latitude</th>
+            <th>Code INSEE</th>
+            <th>Paiement</th>
         </tr>
     </thead>
     <tbody>
         <?php
-            $result = $sparql->query(
-                'SELECT ?station ?long ?lat WHERE { ?station a evcs:ChargingStation . ?station geo:long ?long . ?station geo:lat ?lat .}'
-            );
+            $result = $sparqlChargingStation->query(
+                'SELECT ?stationLabel ?operatorLabel ?long ?lat ?codeINSEE ?paymentModeLabel WHERE { ?station a evcs:ChargingStation. ?station evcs:hasOperator ?operator. ?operator rdfs:label ?operatorLabel. ?station evcs:hasPaymentMode ?paymentMode. ?paymentMode rdfs:label ?paymentModeLabel. ?station rdfs:label ?stationLabel. ?station geo:long ?long. ?station geo:lat ?lat. ?station igeo:codeINSEE ?codeINSEE.}');
+
             foreach ($result as $row) {
                 echo "<tr>" . 
-                        "<td>" . $row->station . "</td>" .
+                        "<td>" . $row->stationLabel . "</td>" .
+                        "<td>" . $row->operatorLabel . "</td>" .
                         "<td>" . $row->long . "</td>" .
                         "<td>" . $row->lat . "</td>" .
+                        "<td>" . $row->codeINSEE . "</td>" .
+                        "<td>" . $row->paymentModeLabel . "</td>" .
                      "</tr>";
+
+                /*
+                $codeINSEE = $sparqlINSEE->query('SELECT DISTINCT ?name WHERE {?town igeo:codeINSEE "' . $row->codeINSEE . '". ?town igeo:nom ?name. }');
+                
+                echo    "<td>" . $codeINSEE[0]->name  . "</td>" . 
+                    "</tr>";
+                */
             }
         ?>
     </tbody>
 </table>
-<p>Total number of countries: <?= $result->numRows() ?></p>
+<p>Total number of rows: <?= $result->numRows() ?></p>
 
 </body>
 </html>
